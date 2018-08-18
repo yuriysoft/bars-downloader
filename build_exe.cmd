70df0c228d9c46f81603a8b4ee3abca7cc61939f
yuriysoft <33071060+yuriysoft@users.noreply.github.com>
2018-08-18 20:58:02 +0000

    Update build_exe.cmd

--- build_exe.cmd
+++ build_exe.cmd
@@ -8,7 +8,7 @@ pyinstaller --noconfirm --log-level=WARN ^
     bars_downloader.py
 copy config.json "dist\config.json"
 copy logging.json "dist\logging.json"
-copy README "dist\README"
+copy README.md "dist\README.md"
 
 rem pip freeze > requirements.txt
 rem pip wheel -w wheels/ -r requirements.txt --pre
